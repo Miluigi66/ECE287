2989926556e922d6906c86c0cd17322ec76667ba
Milo Troup <68747511+Miluigi66@users.noreply.github.com>
2026-09-18 12:25:20 -0400

    Update and rename template.v to Lab2.v

--- Lab/Lab2.v
+++ Lab/Lab2.v
@@ -1,4 +1,4 @@
-module template (input [3:0]SW, output [1:0]LEDR);
+module Lab2 (input [3:0]SW, output [1:0]LEDR);
 	assign LEDR[0] = (SW[0] + SW[1] + SW[2] + SW[3] == 1) | (SW[0] + SW[1] + SW[2] + SW[3] == 3);
 	
 	assign LEDR[1] = (LEDR[0] + SW[0] + SW[1] + SW[2] + SW[3] >= 3);
