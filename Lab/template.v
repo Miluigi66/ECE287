7f8863215c6b7ecdd7d209ac5c1b97da68b55882
Milo Troup <68747511+Miluigi66@users.noreply.github.com>
2026-09-18 12:25:06 -0400

    Add files via upload

---
+++ Lab/template.v
@@ -0,0 +1,7 @@
+module template (input [3:0]SW, output [1:0]LEDR);
+	assign LEDR[0] = (SW[0] + SW[1] + SW[2] + SW[3] == 1) | (SW[0] + SW[1] + SW[2] + SW[3] == 3);
+	
+	assign LEDR[1] = (LEDR[0] + SW[0] + SW[1] + SW[2] + SW[3] >= 3);
+	
+	//(SW[0] & ~SW[1])|(~SW[0] & SW[1]);
+endmodule
